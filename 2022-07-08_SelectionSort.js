1cbef0e59605e74141de3d8d4447256457467361
MCatapang <maocatap@gmail.com>
2022-07-08 12:48:01 -0700

    Initial solution

--- 2022-07-08_SelectionSort.js
+++ 2022-07-08_SelectionSort.js
@@ -20,4 +20,22 @@
 
 const myArr = [3,2,9,5,1,4,8]
 
-function selectionSort(arr){ }
+function selectionSort(arr){ 
+    for(let i=0; i<arr.length; i++) {
+        let min = i;
+        for(let j=i+1; j<arr.length; j++) {
+            if(arr[j] < arr[min]) {
+                min=j;
+            }
+        }
+        if(min != i) {
+            let holder = arr[i];
+            arr[i] = arr[min];
+            arr[min] = holder;
+        }
+    }
+    
+    return arr;
+}
+
+console.log(selectionSort(myArr));
